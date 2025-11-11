3fa191308fe1e9c4618431a8cb0ad07bfdd3ffd9
Matt Creasey <matt.creasey@imosphere.co.uk>
2025-11-11 16:24:30 +1000

    Map viewer fixes

--- src/app/codex/map-viewer/map-viewer.spec.ts
+++ src/app/codex/map-viewer/map-viewer.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MapViewer } from './map-viewer';
+import { MapViewerComponent } from './map-viewer';
 
-describe('MapViewer', () => {
-  let component: MapViewer;
-  let fixture: ComponentFixture<MapViewer>;
+describe('MapViewerComponent', () => {
+  let component: MapViewerComponent;
+  let fixture: ComponentFixture<MapViewerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MapViewer]
+      imports: [MapViewerComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MapViewer);
+    fixture = TestBed.createComponent(MapViewerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
