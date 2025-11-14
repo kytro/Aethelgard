849908bb857a88314cc00c8205196d7d71001c30
Matt Creasey <matt.creasey@imosphere.co.uk>
2025-11-14 20:29:05 +1000

    test update 1

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,25 +1,26 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [AppComponent],
       providers: [provideZonelessChangeDetection()]
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);;
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, codex-admin');
-  });
+it('should render title', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const compiled = fixture.nativeElement as HTMLElement;
+  // The login screen H1 is "Codex Admin"
+  expect(compiled.querySelector('h1')?.textContent).toContain('Codex Admin');
+ });
 });
